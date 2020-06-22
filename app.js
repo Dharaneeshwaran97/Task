bc97995d9e49d28373dfdd8fc43dae5d32d35016
Dharaneeshwaran97 <58076976+Dharaneeshwaran97@users.noreply.github.com>
2020-06-22 21:00:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ db.once("open", function () {
 
 
 app.engine('.html', require('ejs').__express);
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 
 bodyParser = require('body-parser');
